c5db5d9f306fd54b271f35fb55b268fc97f6a96e
aluno <aluno>
2016-06-08 20:59:08 -0300

    Comentarios por linha das Imagens

--- Images.java
+++ Images.java
@@ -9,35 +9,43 @@ public  class Images {
     private BufferedImage linha;
     private BufferedImage energy;
     
+    //Set das imagens das bolas e alocação em uma lista
     public void addBolas(BufferedImage img){
         System.out.println("Importado com sucesso");
         bolas.add(img);  
     }
 
+    //Get da lista das imagens das bolas 
     public ArrayList<BufferedImage> getList(){
         return bolas;
     }
 
+    //Set da imagem da linha na interface do jogo
     public void addLinha(BufferedImage lin){
         linha = lin;
     }
     
+    //Set da fonte usada na interface do jogo
     public void addFont(Font fnt){
         fonte = fnt;
     }
 
+    //Set da imagem da barra de energia do jogador
     public void addEnergy(BufferedImage eng){
         energy = eng;
     }
 
+    //Get da imagem da da barra de energia do jogador
     public BufferedImage getEnergy(){
         return energy;
     }
 
+    //Get da imagem da linha na interface do jogo
     public BufferedImage getLinha(){
         return linha;
     } 
-    
+
+    //Get da fonte usada na interface do jogo
     public Font getFont(){
         return fonte;
     }  
