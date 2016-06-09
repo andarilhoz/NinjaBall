6d7b73248872d547211628a56ca4086b4c222dd1
Magno Gouveia <owlflee@gmail.com>
2016-06-09 00:36:27 +0000

    Adicionado comentario da Tela

--- Screen.java
+++ Screen.java
@@ -43,29 +43,29 @@ public class Screen extends JPanel{
 
     //Set da tela do jogo
     public Screen(int screenSizeX,int screenSizeY,UI u,Images img){
-        screenX = screenSizeX;
-        screenY = screenSizeY;
+        screenX = screenSizeX; //tamanho da tela 
+        screenY = screenSizeY; 
         ui = u;
         imgs = img;
-        frame = new JFrame("GameBalls");
-        frame.setSize(screenSizeX,screenSizeY);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        System.out.println("Screen iniciada");
+        frame = new JFrame("GameBalls"); // nome da janela
+        frame.setSize(screenSizeX,screenSizeY); // define tamanho da janela
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // seta função para fechar janela
+        frame.setVisible(true); // deixa a janela visivel
+        System.out.println("Screen iniciada"); 
         addMouseListener(new MouseAdapter(){
             @Override
-            public void mouseClicked(MouseEvent me){//Set do evento de mouse
+            public void mouseClicked(MouseEvent me){//Checa evento de mouse
                 super.mouseClicked(me);
-                synchronized(bolas){
+                synchronized(bolas){ // Percorre a lista de bolas, e remove caso tenha sido a bola clicada
                     Iterator<Bola> bo = bolas.iterator();
                     while(bo.hasNext()){
                         Bola s = bo.next();
                         if(s.getGraphic().contains(me.getPoint())){
                             synchronized(s){
-                                bo.remove();
-                                s.removeFromCore();
+                                bo.remove(); // remove a bola da lista
+                                s.removeFromCore(); // remove a bola das threads core
                             }
-                            ui.addPoints();
+                            ui.addPoints(); // adiciona pontos pra UI
                         }
                     }
                 }
@@ -107,11 +107,11 @@ public class Screen extends JPanel{
     @Override
     public void paintComponent(Graphics g){//Pinta os componentes da interface
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setColor(Color.BLACK);
-        g2d.fillRect(0,0,getWidth(),getHeight()); 
-        synchronized(bolas){
+        Graphics2D g2d = (Graphics2D) g; // cria o Graphics 2D
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON); // seta o Antialiasing 
+        g2d.setColor(Color.BLACK); // seleciona o preto pra pintar
+        g2d.fillRect(0,0,getWidth(),getHeight());  // pinta o fundo 
+        synchronized(bolas){ // pinta cada uma das bolas
             Iterator<Bola> bo = bolas.iterator();
             while(bo.hasNext()){
               Bola b = bo.next();
@@ -120,19 +120,19 @@ public class Screen extends JPanel{
                 g2d.drawImage(b.getColor(),(int)b.x+3,(int)b.y-3,null);
             }
        }
-       g2d.setPaint(Color.WHITE);
-       Composite c = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .75f);
-       g2d.setComposite(c);
+       g2d.setPaint(Color.WHITE); // seta a cor de branco para escrita
+       Composite c = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .75f); // seta transparencia a 75%
+       g2d.setComposite(c); // adiciona a transparencia pro Graphics2D
        g2d.setFont(new Font("Future Ex",Font.PLAIN, 25));//Set da fonte a ser usada nos textos da interface
        g2d.drawString("Energy ",ui.x,ui.y+10);//Pinta o texto da barra de energia
        for(int en = 0; en < ui.getLifes();en++ ){
-            g2d.drawImage(imgs.getEnergy(),ui.x+(en*22)+105,ui.y-5,null);
+            g2d.drawImage(imgs.getEnergy(),ui.x+(en*22)+105,ui.y-5,null); // mostra as imagens de energia pra cada vida que o player tiver
        }
        g2d.drawString(ui.getPoints()+"pts",ui.x+600,ui.y+10);//Pinta o texto da pontuação
-       Composite co = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .4f);
-       g2d.setComposite(co);
+       Composite co = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .4f); // seta transparencia a 4%
+       g2d.setComposite(co); // adiciona transparencia no Graphics2D
        g2d.drawString("Do not touch this line",ui.x+150,ui.y+320);//Pinta o texto da instrução do jogo
-       if(!ui.getAlive()){
+       if(!ui.getAlive()){ //se o Jogador perder mostra texto de Game Over
            g2d.setFont(new Font(g2d.getFont().getFontName(),Font.PLAIN, 60));
            g2d.drawString("Voce perdeu!",ui.x+100,ui.y+180);//Pinta a notificação de derrota quando ojogador perde
        }
