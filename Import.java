850ab1ca5f0b3f144be762bcf73c421232acc4d3
Magno Gouveia <owlflee@gmail.com>
2016-06-09 00:25:26 +0000

    Adicionado comentarios da thread import

--- Import.java
+++ Import.java
@@ -25,33 +25,35 @@ public class Import implements Runnable{
     
     @Override
     public void run(){
-        Thread thread = Thread.currentThread();
-        System.out.println("Thread: "+thread.getName()+" iniciada");
-        if(option == 0){
+        Thread thread = Thread.currentThread(); // Pega this thread
+        System.out.println("Thread: "+thread.getName()+" iniciada");// mostra o nome da thread iniciada
+        // dependendo da option é um tipo de arquivo
+		// imagem da bola, imagem de UI ou fonte
+		if(option == 0){
             try{
                 image = ImageIO.read(new File(path));
-                synchronized(imgs){imgs.addBolas(image);}
+                synchronized(imgs){imgs.addBolas(image);} // adiciona imagem de bola na lista array
             }catch(IOException e){
                 e.printStackTrace();
             }   
         }else if(option == 1){
             try{
                 image = ImageIO.read(new File(path));
-                synchronized(imgs){imgs.addEnergy(image);}
+                synchronized(imgs){imgs.addEnergy(image);} // adiciona imagem da energia
             }catch(IOException e){
                 e.printStackTrace();
             }
         }else if(option == 2){
             try{
                 GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-                ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(path)));
+                ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(path)));// importa e registra a fonte no sistema
             }catch(IOException | FontFormatException e){
                 e.printStackTrace();
             }
         }else if(option == 3){
              try{
                 image = ImageIO.read(new File(path));
-                synchronized(imgs){imgs.addLinha(image);}
+                synchronized(imgs){imgs.addLinha(image);}//adiciona imagens das linhas
             }catch(IOException e){
                 e.printStackTrace();
             }
